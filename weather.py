2096d5fa2a262d12070cc6e17bab480ed8d88291
nicksansouci@gmail.com <nicksansouci@gmail.com>
2022-02-03 14:36:50 -0700

    Updating buttons

--- weather.py
+++ weather.py
@@ -10,15 +10,15 @@ canvas = tk.Canvas(root, height=height, width=width)
 canvas.pack()
 
 frame = tk.Frame(root, bg='white')
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.5)
 
-button = tk.Button(frame, text="First Button")
-button.pack()
+button = tk.Button(frame, text="First Button", fg='red', bg='white')
+button.place(relx=0.85, rely=0.20, relwidth=0.25, relheight=0.25)
 
 label = tk.Label(frame, text="This is a test label", bg="blue")
-label.pack()
+label.place(relx=0.3, rely=0, relwidth=0.45, relheight=0.25)
 
 entry = tk.Entry(frame, bg='gray')
-entry.pack()
+entry.grid(row=0, column=2)
 
 root.mainloop()
