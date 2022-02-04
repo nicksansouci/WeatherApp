231b8099054ddba5bee461fa64c1be7d64e7310f
nicksansouci@gmail.com <nicksansouci@gmail.com>
2022-02-04 11:45:11 -0700

    Finished API

--- weather.py
+++ weather.py
@@ -1,5 +1,33 @@
 import tkinter as tk
+from tkinter import font
 import requests
+
+def print_response(weather):
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
+        final_entry = 'City: %s \nConditions: %s \nTemperature: (ºF): %s' % (name, desc, temp) 
+    except:
+        final_entry = "Please type City Name"
+    return final_entry
+    
+
+def view_weather(city):
+    weather_key = '4e44dce2fb0e793b24944d695904d7bc'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+    # GET request for API
+    response = requests.get(url, params=params)
+    #parameters=parameters
+    weather = response.json()
+    label['text'] = print_response(weather)
+
+    #print(weather['name'])
+    #print(weather['weather'][0]['description'])
+    #print(weather['main']['temp'])
+
+#e92bfde834b2ca9cf0cb67dcbe6b6038
 
 height = 600
 width = 800
@@ -18,10 +46,10 @@ background_label.place(relwidth=1, relheight=1)
 frame = tk.Frame(root, bg='#c7ddcc', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.70, relheight=0.09, anchor='n')
 
-entry = tk.Entry(frame, text='City or ZIP Code', bg='#16123f', font=40, fg='#ffe26a')
+entry = tk.Entry(frame, text='City or ZIP Code', bg ='#16123f', font=40, fg='#ffe26a')
 entry.place(relwidth=0.65,relheight=1)
 
-button = tk.Button(frame, text="View Weather", font=40, bg='#16123f', fg='#16123f')
+button = tk.Button(frame, text="View Weather", font=40, bg='#16123f', fg='#16123f', command = lambda: view_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 #label = tk.Label(frame, text="This is a test label", bg="blue")
@@ -29,7 +57,7 @@ button.place(relx=0.7, relheight=1, relwidth=0.3)
 lowerframe = tk.Frame(root, bg='#c7ddcc', bd=10)
 lowerframe.place(relx = 0.5, rely = 0.25, relwidth = 0.75, relheight = 0.6, anchor = 'n')
 
-label = tk.Label(lowerframe)
+label = tk.Label(lowerframe, font=('Forte', 20))
 label.place(relwidth=1, relheight=1)
 
 root.mainloop()
